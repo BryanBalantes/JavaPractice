984d97e784508826f9ee2fd4ec9906917e9238cc
BryanBalantes <bryanbalantes22@gmail.com>
2026-06-28 20:25:55 +0800

    update printf file

--- outputMethods/printf.java
+++ outputMethods/printf.java
@@ -49,12 +49,11 @@ public class Printf {
 
        Scanner sc=new Scanner(System.in);
        System.out.println("================================");
-       for(int i=0;i<3;i++){
+       for(int i=0;i<3;i++) {
            String s1=sc.next();
            int x=sc.nextInt();
            System.out.printf("%-15s%03d%n", s1, x);
         }
        System.out.println("================================");
-
   }
 }
